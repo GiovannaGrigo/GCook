1cb1c368f94975b90a55c6e04f5797a8f591bfea
GiovannaGrigo <giovanna1grigo@gmail.com>
2024-10-24 22:25:37 -0300

    Controllers, Index, Account e wwroot

--- GCook/Controllers/AccountController.cs
+++ GCook/Controllers/AccountController.cs
@@ -53,6 +53,42 @@ public class AccountController : Controller
         return RedirectToAction("Index", "Home");
     }
 
+    [HttpGet]
+    public IActionResult Registro()
+    {
+        RegistroVM register = new();
+        return View(register);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Registro(RegistroVM register)
+    {
+        register.Enviado = false;
+        if (ModelState.IsValid)
+        {
+            var result = await _usuarioService.RegistrarUsuario(register);
+            if (result != null)
+                foreach (var error in result)
+                {
+                    ModelState.AddModelError(string.Empty, error);
+                }
+            register.Enviado = result == null;
+        }
+        return View(register);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> ConfirmarEmail(string userId, string code)
+    {
+        if (userId == null || code == null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        await _usuarioService.ConfirmarEmail(userId, code);
+        return View(true);
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
